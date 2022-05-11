4bd377e241833db9e0dd3aeb7fb0e2d3859248e9
LucyMoon <81845462+LucyMoon@users.noreply.github.com>
2022-05-11 10:30:35 +0900

    내 정보창 완성

--- app/src/main/java/kr/hs/dion_kotlin/MainActivity.kt
+++ app/src/main/java/kr/hs/dion_kotlin/MainActivity.kt
@@ -2,10 +2,37 @@ package kr.hs.dion_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    val Bn_main : BottomNavigationView by lazy{
+        findViewById(R.id.Bn_Main)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Bn_main.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.Action_Event -> {
+                    replaceFragment(EventFragment())
+                }
+                R.id.Action_Myinfo -> {
+                    replaceFragment(MyinfoFragment())
+                }
+            }
+            true
+        }
+        Bn_main.selectedItemId = R.id.Action_Event
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fl_main, fragment)
+        fragmentTransaction.commit()
     }
 }
